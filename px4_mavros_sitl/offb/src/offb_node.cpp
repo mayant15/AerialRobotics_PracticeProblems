c1a2c93d897728e289392fd5e6bc8ca6e2bb917d
Mayant Mukul <mayantmukul@hotmail.com>
2018-04-10 22:26:01 +0530

    Problem 1

--- px4_mavros_sitl/offb/src/offb_node.cpp
+++ px4_mavros_sitl/offb/src/offb_node.cpp
@@ -10,11 +10,29 @@
 
 #include <cmath>
 
+#define THRESHOLD 0.1
+#define STEP 0.1
+
 mavros_msgs::State current_state;
+geometry_msgs::PoseStamped current_pose;
+
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
 }
 
+void pose_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
+    current_pose = *msg;
+}
+
+float distance(const geometry_msgs::PoseStamped::ConstPtr& curr_pose,
+        const geometry_msgs::PoseStamped::ConstPtr& reqd_pose){
+    float x1 = curr_pose->pose.position.x;
+    float y1 = curr_pose->pose.position.y;
+    float x2 = reqd_pose->pose.position.x;
+    float y2 = reqd_pose->pose.position.y;
+
+    return sqrt((x1 - x2)*(x1 - x2) + (y1 -  y2)*(y1 - y2));
+}
 
 int main(int argc, char *argv[])
 {
@@ -23,6 +41,8 @@ int main(int argc, char *argv[])
 
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
         ("mavros/state", 10, state_cb);
+    ros::Subscriber pose_sub = nh.subscribe<geometry_msgs::PoseStamped>
+        ("mavros/local_position/pose", 10, pose_cb);
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
         ("mavros/setpoint_position/local", 10);
     ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
@@ -42,15 +62,15 @@ int main(int argc, char *argv[])
     }
 
 
-    geometry_msgs::PoseStamped pose;
-    pose.pose.position.x = 0;
-    pose.pose.position.y = 0;
-    pose.pose.position.z = 2;
+    geometry_msgs::PoseStamped reqd_pose;
+    reqd_pose.pose.position.x = 0;
+    reqd_pose.pose.position.y = 0;
+    reqd_pose.pose.position.z = 2;
 
 
     // send a few setpoints before starting
     for (int i = 100; i > 0 && ros::ok(); i--) {
-        local_pos_pub.publish(pose);
+        local_pos_pub.publish(reqd_pose);
         ros::spinOnce();
         rate.sleep();
     }
@@ -86,14 +106,17 @@ int main(int argc, char *argv[])
             }
         }
 
+        // Check distance from current position to required position
+        // Update required pose if near
+        if (current_state.mode == "OFFBOARD" && current_state.armed &&
+                distance(&current_pose, &reqd_pose) < THRESHOLD) {
+            reqd_pose.pose.position.x += STEP;
+            reqd_pose.pose.position.y += sin(reqd_pose.pose.position.x);
+            ROS_INFO("POSE UPDATED");
+        }
 
-        local_pos_pub.publish(pose);
 
-        pose.pose.position.x += 0.1;
-        pose.pose.position.y += sin(pose.pose.position.x);
-        pose.pose.position.z = 2;
-
- 
+        local_pos_pub.publish(reqd_pose);
         ros::spinOnce();
         rate.sleep();
     }
